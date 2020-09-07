e7dcc3c50658e6700e8c344d6ff57b7b70dd2fc8
sleny <xkqortjddl@naver.com>
2020-09-08 01:43:44 +0900

    corsadd

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,6 @@ var express = require("express");
 var router = express.Router();
 const request = require("request");
 const parser = require("xml2json");
-const { render } = require("../app");
 
 const HOST = "http://api.nongsaro.go.kr/service/garden/gardenList";
 const requestUrl = `${HOST}?apiKey=20200206NNRF9K4P2NRBPWZJ2RC8GW&&numOfRows=10`;
