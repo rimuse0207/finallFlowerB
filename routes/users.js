11ff4ff863b2b6234ac2d8f8c4eb57ea8cd2dd0e
sleny <xkqortjddl@naver.com>
2020-09-08 01:06:54 +0900

    addFirstData

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,34 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const request = require("request");
+const parser = require("xml2json");
+
+const HOST = "http://api.nongsaro.go.kr/service/garden/gardenList";
+const requestUrl = `${HOST}?apiKey=20200206NNRF9K4P2NRBPWZJ2RC8GW&&numOfRows=217`;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", async (req, res) => {
+  // res.header("Access-Control-Allow-Origin", "*");
+  // res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+  // res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+  // res.header("Access-Control-Allow-Credentials", true);
+  try {
+    let data = null;
+    await request(
+      {
+        url: requestUrl,
+        method: "GET",
+      },
+      (error, response, xml) => {
+        const json = JSON.parse(parser.toJson(xml));
+        data = json.response.body.items.item;
+        console.log(requestUrl);
+        res.json({ data: data });
+      }
+    );
+  } catch (error) {
+    console.log("asdasd", error);
+  }
 });
 
 module.exports = router;
