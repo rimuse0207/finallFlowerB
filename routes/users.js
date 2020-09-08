70c3e98a0e93703827839e428ad9aa6769f89272
sleny <xkqortjddl@naver.com>
2020-09-08 13:53:58 +0900

    deploy

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,9 @@ const request = require("request");
 const parser = require("xml2json");
 
 const HOST = "http://api.nongsaro.go.kr/service/garden/gardenList";
-const requestUrl = `${HOST}?apiKey=20200206NNRF9K4P2NRBPWZJ2RC8GW&&numOfRows=10`;
+const requestUrl = `${HOST}?apiKey=20200206NNRF9K4P2NRBPWZJ2RC8GW&&numOfRows=127`;
+const HOST2 = "http://api.nongsaro.go.kr/service/garden/gardenDtl";
+const requestUrl2 = `${HOST2}?apiKey=${process.env.APIKEY}&&cntntsNo=`;
 let data = null;
 const datas = request(
   {
@@ -20,13 +22,28 @@ const datas = request(
 
 /* GET users listing. */
 router.get("/", (req, res) => {
-  // res.header("Access-Control-Allow-Origin", "*");
-  // res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
-  // res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-  // res.header("Access-Control-Allow-Credentials", true);
-  console.log(data);
-
   res.json({ data: data });
 });
 
+router.post("/qwe", async (req, res) => {
+  try {
+    let data3 = null;
+    let number = req.body.number;
+    console.log(number);
+    await request(
+      {
+        url: `${requestUrl2}${req.body.number}`,
+        method: "GET",
+      },
+      (error, response, xml) => {
+        const json = JSON.parse(parser.toJson(xml));
+        data3 = json.response.body.item;
+        console.log(data3);
+        res.json(data3);
+      }
+    );
+  } catch (err) {
+    console.log("Detail error ", err);
+  }
+});
 module.exports = router;
